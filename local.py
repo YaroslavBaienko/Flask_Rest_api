f72c97449e19148cab334bf2398739fb628df7e2
YaroslavBaienko <zerhug@gmail.com>
2023-08-06 17:22:00 +0300

    second changes

--- local.py
+++ local.py
@@ -1,9 +1,7 @@
 import requests
 
-
-res = requests.get("http://127.0.0.1:3000/api/main")
-print(res.json())
-
-
-
-
+# res = requests.get("http://127.0.0.1:3000/api/main")
+res_courses = requests.post("http://127.0.0.1:3000/api/courses", json={"name": "Dj", "videos": 662})
+# res_courses = requests.get("http://127.0.0.1:3000/api/courses/0")
+# res_courses = requests.delete("http://127.0.0.1:3000/api/courses/24", json={"name": "GOLANG", "videos": 3})
+print(res_courses.json())
--- main.py
+++ main.py
@@ -1,17 +1,52 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, render_template
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api()
 
+courses = {1: {"name": "Python", "videos": 15},
+           2: {"name": "Java", "videos": 10},
+           3: {"name": "C#", "videos": 2},
+           4: {"name": "Java Script", "videos": 1}
+           }
+
+parser = reqparse.RequestParser()
+parser.add_argument("name", type=str, required=True)
+parser.add_argument("videos", type=int, required=True)
+
 
 class Main(Resource):
-    def get(self):
-        return {"info": "Some info", "num": 56}
+    def get(self, course_id=None):
+        if course_id is None:
+            return courses
+        else:
+            return courses[course_id]
+
+    def delete(self, course_id):
+        if course_id not in courses:
+            abort(404, message=f"Course with course_id {course_id} not found")
+        del courses[course_id]
+        return courses
+
+    def post(self):
+        new_course = parser.parse_args()
+        course_id = max(int(v) for v in courses.keys()) + 1
+        courses[course_id] = new_course
+        return courses, 201
+
+    def put(self, course_id):
+        courses[course_id] = parser.parse_args()
+        return courses
 
 
-api.add_resource(Main, "/api/main")
+api.add_resource(Main, "/api/courses/<int:course_id>", "/api/courses")
 api.init_app(app)
+
+
+@app.get("/")
+def index():
+    return render_template()
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000, host="127.0.0.1")
